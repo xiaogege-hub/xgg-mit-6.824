b1fb6ad7d3cb47c17fa262f2753413dda0a3f9f4
jiajun.sun <jiajun.sun@horizon.ai>
2023-05-03 15:42:46 +0800

    add buttonrpc lib

--- MapReduce/master.cpp
+++ MapReduce/master.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "./buttonrpc-master/buttonrpc.hpp"
 
 using namespace std;
 
-int main() { return 0; }
+int main(int argc, char* argv[]) { 
+    if (argc < 2) {
+        cout << "missing parameter! The format is ./Master pg*.txt" << endl;
+        exit(-1);
+    }
+    buttonrpc server;
+    
+    
+    return 0; 
+}
